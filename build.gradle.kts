16a9c92e672f63193b4f59ec0d67bc7ed8ab4f81
Alan Andersen <alanderr@gmail.com>
2020-08-14 15:44:07 -0600

    add instruction for building with local jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,10 +15,14 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.8")
     implementation("io.vertx:vertx-lang-kotlin-coroutines:$vertxVersion")
     implementation("io.vertx:vertx-lang-kotlin:$vertxVersion")
-    implementation("io.vertx:vertx-sql-client:3.9.0")
     implementation("io.vertx:vertx-core:$vertxVersion")
     implementation("io.vertx:vertx-web:$vertxVersion")
     implementation("io.vertx:vertx-pg-client:$vertxVersion")
+
+
+    implementation("io.vertx:vertx-sql-client:$vertxVersion")
+    // comment out the above line and put your own path below for local jar, then ctrl-shift-o to refresh gradle projects
+//    implementation(files("/home/alan/dev/3rdParty/vertx-sql-client/vertx-sql-client/target/vertx-sql-client-3.9.0.jar"))
 }
 
 
